481cde921268a2851c367edd73a95f6cebd121a2
Anatolio Nikiforidis <nikiforova693@gmail.com>
2022-01-15 07:51:06 +0500

    feat: utility function added

--- vpnmauth/__init__.py
+++ vpnmauth/__init__.py
@@ -1,3 +1,9 @@
 from ._api import VpnmApiClient
 
 __version__ = "0.1.0"
+
+
+def get_hostname_or_address(node: dict):
+    if node["server"][0][1] == "443":
+        return node["server"][1]["host"]
+    return node["server"][0][0]
